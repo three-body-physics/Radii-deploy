5258e55e4d48d1ce91168f344d86589c64e07b86
three-body-physics <mintpie@googlemail.com>
2023-11-20 19:40:38 +0000

    Update secret.js
    remove mongo access key

--- backend/controller/secret.js
+++ backend/controller/secret.js
@@ -1,5 +1,5 @@
 module.exports = {
 	key: "A secret is best kept secret",
-	mongolabURL: "mongodb+srv://CandyCasper:wXZaClf7UGBghYVv@radii.zornxbo.mongodb.net/?retryWrites=true&w=majority",
+	mongolabURL: "mongodb+srv://@radii.zornxbo.mongodb.net/?retryWrites=true&w=majority",
 	sessionKey: "life gives you lemons"	
 }
